796ed9212239a6f20011ca998ebab97d1e44b07a
kedric <hgkim2@ntels.com>
2022-02-21 11:18:40 +0900

    jpa bookshop

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,10 +1,9 @@
-package hellojpa;
+package jpabook.jpashop;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.awt.*;
 
 public class JpaMain {
 
@@ -17,27 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try{
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("helloB");
-
-            //조회
-            Member findMember = em.find(Member.class, 1L);
-
-            //수정
-            findMember.setName("helloJ");
-
-            //삭제
-//            em.remove(findMember);
-
-            //추가
-//            em.persist(member);
             tx.commit();
-        }catch (Exception e){
+        }catch(Exception e){
             tx.rollback();
         }finally {
             em.close();
         }
+
         emf.close();
     }
 }
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private  String name;
+    private  String city;
+    private  String street;
+    private  String zipcode;
+}
